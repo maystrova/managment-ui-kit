729b87709f83d09c1832270df89b1efd2845cbe6
Kate Maystrova <maystrovakate@gmail.com>
2021-04-04 20:51:58 +0300

    Update src/Components/Uploading/style.tsx
    Co-authored-by: Ilya Pasuyk <pasyuk.com@gmail.com>

--- src/Components/Uploading/style.tsx
+++ src/Components/Uploading/style.tsx
@@ -1,7 +1,6 @@
 import styled, { keyframes } from 'styled-components'
 
 const StyledUploading = styled.div`
-    //position: relative;
     width: 100%;
     display: flex;
     justify-content: center;
